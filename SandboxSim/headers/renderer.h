9495eda77f2bc3128fd5447f5bb54327f7924758
Hubert Blonowski <135438393+sKsieciunio@users.noreply.github.com>
2024-04-04 12:24:10 +0200

    resolved linker errors

--- SandboxSim/headers/renderer.h
+++ SandboxSim/headers/renderer.h
@@ -1,32 +1,12 @@
 #pragma once
-#include <glad/glad.h>
-#include "shader.h"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 extern const float scale;
 extern const int gridSize;
 
+class Shader;
+
 namespace Renderer
 {
-
-	void renderRecGrid(Shader& shader, int posX, int posY) {
-		shader.use();
-		glm::mat4 model = glm::identity<glm::mat4>();
-		model = glm::scale(model, glm::vec3(scale));
-		model = glm::translate(model, glm::vec3(1.0f, -1.0f, 0.0f));
-		model = glm::translate(model, glm::vec3(
-			-static_cast<float>(gridSize) + 2 * posX,
-			static_cast<float>(gridSize) - 2 * posY,
-			0.0f
-		));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-	}
-
-
-	void clear() {
-		glClearColor(0.12f, 0.14f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
+	void renderRecGrid(Shader& shader, int posX, int posY);
+	void clear();
 }
---
+++ SandboxSim/src/renderer.cpp
@@ -0,0 +1,25 @@
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.h"
+#include "../headers/renderer.h"
+
+void Renderer::renderRecGrid(Shader& shader, int posX, int posY) {
+	shader.use();
+	glm::mat4 model = glm::identity<glm::mat4>();
+	model = glm::scale(model, glm::vec3(scale));
+	model = glm::translate(model, glm::vec3(1.0f, -1.0f, 0.0f));
+	model = glm::translate(model, glm::vec3(
+		-static_cast<float>(gridSize) + 2 * posX,
+		static_cast<float>(gridSize) - 2 * posY,
+		0.0f
+	));
+	shader.setMat4("model", model);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void Renderer::clear() {
+	glClearColor(0.12f, 0.14f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
